acab67efb3beb22185269e913bc6fee1dee12cc2
Gulshan <abhinavg90834@gmail.com>
2024-03-21 12:30:04 +0530

    seeder file fixed

--- server/src/seeder.ts
+++ server/src/seeder.ts
@@ -1,12 +1,42 @@
-import IndustryService from "./services/Industry.service";
+import Industry from "./models/Industry.model";
 import Data from "./utils/Data";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
 
-async function addIndustryListToDB() {
+async function main() {
   try {
-    await IndustryService.createManyIndustry(Data.IndustryList);
-  } catch (e: any) {
-    console.log(e);
+    const MONGO_URI =
+      process.env.MONGO_URI ||
+      "mongodb://root:rootpassword@mongodb_server:27017/tms?authSource=admin";
+    await mongoose.connect(MONGO_URI);
+    console.log("Connected to the database successfully");
+
+    await addIndustryListToDB();
+    console.log("Data added successfully");
+  } catch (error) {
+    console.error("Error:", error);
+    process.exit(1);
+  } finally {
+    mongoose.disconnect();
+    console.log("Disconnected from the database");
+    process.exit(0);
   }
 }
 
-addIndustryListToDB();
+async function addIndustryListToDB() {
+  try {
+    await Industry.insertMany(Data.IndustryList, {
+      ordered: false,
+    });
+    console.log("Data inserted successfully:");
+  } catch (error: any) {
+    if (error.code === 11000) {
+      console.error("Duplicate key error:", error);
+    } else {
+      throw error;
+    }
+  }
+}
+
+main();
