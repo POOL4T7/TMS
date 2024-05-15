bfd8486307c8b8998453350bfe3c0e176d0bb896
Gulshan <abhinavg90834@gmail.com>
2024-05-15 11:54:42 +0530

    user and roles tables fixed

--- client/src/screens/Company/Position/index.tsx
+++ client/src/screens/Company/Position/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from 'react';
 import {
   Box,
   IconButton,
@@ -12,50 +12,54 @@ import {
   TablePagination,
   TableRow,
   Alert,
-} from "@mui/material";
-import { Delete } from "@mui/icons-material";
+} from '@mui/material';
+import { Delete } from '@mui/icons-material';
 import {
   useDeletePostMutation,
   usePositionListQuery,
-} from "../../../redux/services/position";
-import TableToolBar from "../../../components/TableToolBar";
-import Loader from "../../../components/Loader";
-import AddPosition from "../../../components/Position/AddPosition";
-import { ErrorType } from "../../../models/custom";
+} from '../../../redux/services/position';
+import TableToolBar from '../../../components/TableToolBar';
+import Loader from '../../../components/Loader';
+import AddPosition from '../../../components/Position/AddPosition';
+import { ErrorType } from '../../../models/custom';
 
 interface Column {
-  id: "id" | "name" | "totalMembers" | "status" | "action" | "team";
+  // sno: 'sno';
+  id: 'id' | 'name' | 'totalMembers' | 'status' | 'action' | 'team' | 'sno';
   label: string;
   maxWidth?: number;
-  align?: "right";
+  align?: 'right';
   status?: string;
   showOrder: boolean;
 }
 
 const columns: readonly Column[] = [
-  { id: "id", label: "#id", maxWidth: 100, showOrder: true },
-  { id: "name", label: "Name", maxWidth: 100, showOrder: true },
-  { id: "team", label: "Team Name", maxWidth: 100, showOrder: false },
+  { id: 'sno', label: 'S No.', maxWidth: 65, showOrder: true },
+  { id: 'id', label: '#id', maxWidth: 100, showOrder: true },
+  { id: 'name', label: 'Name', maxWidth: 100, showOrder: true },
   {
-    id: "totalMembers",
-    label: "Total Members",
+    id: 'team',
+    label: 'Team Name',
     maxWidth: 100,
     showOrder: false,
   },
   {
-    id: "status",
-    label: "Status",
-    showOrder: true,
+    id: 'totalMembers',
+    label: 'Total Members',
+    maxWidth: 100,
+    showOrder: false,
   },
+  { id: 'status', label: 'Status', showOrder: true },
 ];
-type Order = "asc" | "desc";
+
+type Order = 'asc' | 'desc';
 
 export default function Position() {
-  console.log("Position page is rendering");
+  console.log('Position page is rendering');
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
-  const [order, setOrder] = useState<Order>("desc");
-  const [orderBy, setOrderBy] = useState<string>("id");
+  const [order, setOrder] = useState<Order>('desc');
+  const [orderBy, setOrderBy] = useState<string>('id');
   const { data, isFetching, isLoading, error, isError } = usePositionListQuery({
     page: page + 1,
     rowsPerPage,
@@ -82,8 +86,8 @@ export default function Position() {
   };
 
   const handleRequestSort = (property: string) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
     setOrderBy(property);
   };
 
@@ -99,7 +103,7 @@ export default function Position() {
   return (
     <Box
       sx={{
-        margin: "15px",
+        margin: '15px',
       }}
     >
       {isLoading && <Loader size={100} thickness={1.5} />}
@@ -107,28 +111,29 @@ export default function Position() {
         <Alert severity="error">{(error as ErrorType)?.message}</Alert>
       )}
       {data && (
-        <Paper sx={{ width: "100%", overflow: "hidden" }}>
+        <Paper sx={{ width: '100%', overflow: 'hidden' }}>
           <TableToolBar
             numSelected={0}
             title="Position & Roles"
             Children={<AddPosition positionId="" />}
             toolTipText="Add Position"
           />
-          <TableContainer sx={{ maxHeight: 440 }}>
+          <TableContainer sx={{ maxHeight: '74vh' }}>
             <Table stickyHeader aria-label="sticky table">
-              <caption>Team listing with stats</caption>
               <TableHead>
                 <TableRow>
+                  {/* <TableCell align="left">Action</TableCell> */}
                   {columns.map((column) => (
                     <TableCell
                       key={column.id}
                       align={column.align}
+                      style={{ maxWidth: `${column.maxWidth}px` }}
                       // sortDirection={orderBy === column.id ? order : "asc"}
                     >
                       {column.showOrder ? (
                         <TableSortLabel
                           active={orderBy === column.id}
-                          direction={orderBy === column.id ? order : "asc"}
+                          direction={orderBy === column.id ? order : 'asc'}
                           onClick={() => handleRequestSort(column.id)}
                         >
                           {column.label}
@@ -141,23 +146,17 @@ export default function Position() {
                   <TableCell align="left">Action</TableCell>
                 </TableRow>
               </TableHead>
-              {/* <TableBody>
-                <TableRow sx={{}}>
-                  <TableCell>
-                    {true && <Loader size={50} thickness={2} />}
-                  </TableCell>
-                </TableRow>
-              </TableBody> */}
               <TableBody>
                 {!isFetching &&
-                  data.positionList?.map((row) => {
+                  data.positionList?.map((row, idx) => {
                     return (
                       <TableRow hover tabIndex={-1} key={row._id}>
+                        <TableCell>{10 * page + idx + 1}</TableCell>
                         <TableCell>
                           {row._id.substring(row._id.length - 6)}
                         </TableCell>
                         <TableCell>{row.name}</TableCell>
-                        <TableCell>{row.team.name || "null"}</TableCell>
+                        <TableCell>{row.team.name || 'null'}</TableCell>
                         <TableCell>{row.totalMembers || 0}</TableCell>
                         <TableCell>{row.status}</TableCell>
                         <TableCell>
--- client/src/screens/Company/Users/index.tsx
+++ client/src/screens/Company/Users/index.tsx
@@ -1,64 +1,73 @@
-import { useState } from "react";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
-import { Alert, Box, TableSortLabel } from "@mui/material";
-import TableToolBar from "../../../components/TableToolBar";
-import Loader from "../../../components/Loader";
-import { useUserListQuery } from "../../../redux/services/user";
-import AddUpdate from "../../../components/Users/AddUpdate";
-import { ErrorType } from "../../../models/custom";
+import { useState } from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import { Alert, Box, TableSortLabel } from '@mui/material';
+import TableToolBar from '../../../components/TableToolBar';
+import Loader from '../../../components/Loader';
+import { useUserListQuery } from '../../../redux/services/user';
+import AddUpdate from '../../../components/Users/AddUpdate';
+import { ErrorType } from '../../../models/custom';
 
 interface Column {
-  id: "id" | "name" | "position" | "status" | "action" | "team" | "employeId";
+  id:
+    | 'id'
+    | 'name'
+    | 'position'
+    | 'status'
+    | 'action'
+    | 'team'
+    | 'employeId'
+    | 'sno';
   label: string;
   maxWidth?: number;
-  align?: "right" | "center";
+  align?: 'right' | 'center';
   status?: string;
   showSorting: boolean;
 }
 
 const columns: readonly Column[] = [
-  { id: "id", label: "#id", maxWidth: 100, showSorting: true },
-  { id: "name", label: "Name", maxWidth: 100, showSorting: false },
+  { id: 'sno', label: 'S No.', maxWidth: 65, showSorting: true },
+  { id: 'id', label: '#id', maxWidth: 100, showSorting: true },
+  { id: 'name', label: 'Name', maxWidth: 100, showSorting: false },
   {
-    id: "team",
-    label: "Team Name",
+    id: 'team',
+    label: 'Team Name',
     maxWidth: 100,
     showSorting: false,
     // align: "center",
   },
   {
-    id: "position",
-    label: "Position",
+    id: 'position',
+    label: 'Position',
     maxWidth: 100,
     showSorting: false,
   },
   {
-    id: "employeId",
-    label: "EmployeId",
+    id: 'employeId',
+    label: 'EmployeId',
     maxWidth: 100,
     showSorting: true,
   },
   {
-    id: "status",
-    label: "Status",
+    id: 'status',
+    label: 'Status',
     showSorting: true,
   },
 ];
-type Order = "asc" | "desc";
+type Order = 'asc' | 'desc';
 
 export default function User() {
-  console.log("Users page is rendering");
+  console.log('Users page is rendering');
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
-  const [order, setOrder] = useState<Order>("asc");
-  const [orderBy, setOrderBy] = useState<string>("id");
+  const [order, setOrder] = useState<Order>('asc');
+  const [orderBy, setOrderBy] = useState<string>('id');
   const { data, isFetching, isLoading, isError, error } = useUserListQuery({
     page,
     rowsPerPage,
@@ -81,14 +90,14 @@ export default function User() {
     setPage(0);
   };
   const handleRequestSort = (property: string) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
     setOrderBy(property);
   };
   return (
     <Box
       sx={{
-        margin: "15px",
+        margin: '15px',
       }}
     >
       {isLoading && <Loader size={100} thickness={1.5} />}
@@ -96,14 +105,14 @@ export default function User() {
         <Alert severity="error">{(error as ErrorType).message}</Alert>
       )}
       {data && (
-        <Paper sx={{ width: "100%", overflow: "hidden" }}>
+        <Paper sx={{ width: '100%', overflow: 'hidden' }}>
           <TableToolBar
             numSelected={0}
             title="Users"
-            Children={<AddUpdate userId={""} />}
+            Children={<AddUpdate userId={''} />}
             toolTipText="Add User"
           />
-          <TableContainer sx={{ maxHeight: 440 }}>
+          <TableContainer sx={{ maxHeight: '74vh' }}>
             <Table stickyHeader aria-label="sticky table">
               <caption>Team listing with stats</caption>
               <TableHead>
@@ -112,11 +121,12 @@ export default function User() {
                     <TableCell
                       key={column.id}
                       align={column.align}
-                      sortDirection={orderBy === column.id ? order : "asc"}
+                      sortDirection={orderBy === column.id ? order : 'asc'}
+                      style={{ maxWidth: `${column.maxWidth}px` }}
                     >
                       <TableSortLabel
                         active={orderBy === column.id && column.showSorting}
-                        direction={orderBy === column.id ? order : "asc"}
+                        direction={orderBy === column.id ? order : 'asc'}
                         onClick={() => handleRequestSort(column.id)}
                       >
                         {column.label}
@@ -129,16 +139,17 @@ export default function User() {
               {/* {true && <Loader size={50} thickness={2} />} */}
               <TableBody>
                 {(!isFetching || !isLoading) &&
-                  data.userList?.map((row) => {
+                  data.userList?.map((row, idx) => {
                     return (
                       <TableRow hover tabIndex={-1} key={row._id}>
+                        <TableCell>{10 * page + idx + 1}</TableCell>
                         <TableCell>
                           {row._id.substring(row._id.length - 6)}
                         </TableCell>
                         <TableCell>{`${row.firstName} ${row.lastName}`}</TableCell>
-                        <TableCell>{row.departmentId?.name || "-"}</TableCell>
-                        <TableCell>{row.positionId?.name || "-"}</TableCell>
-                        <TableCell>{row.employeeId || "-"}</TableCell>
+                        <TableCell>{row.departmentId?.name || '-'}</TableCell>
+                        <TableCell>{row.positionId?.name || '-'}</TableCell>
+                        <TableCell>{row.employeeId || '-'}</TableCell>
                         <TableCell>{row.status}</TableCell>
                         <TableCell>
                           <AddUpdate userId={row._id} />
